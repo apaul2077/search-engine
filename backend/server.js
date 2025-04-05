24555684cddf616c90f5740b71aba2d362bf50bd
Avishek Paul <avishekpaul814@gmail.com>
2025-04-05 17:14:58 +0530

    deployment changes 2

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: "https://search-engine-400.netlify.app/", 
+  origin: "https://search-engine-400.netlify.app", 
   credentials: true
 }));
 
